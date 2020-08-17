850060fab876e0e53d65086805462bafab2d99a4
Shahzod Makhmudov <65654780+Shahzod-CBU@users.noreply.github.com>
2020-08-17 23:25:08 +0500

    Update sankey qpm.R

--- sankey qpm.R
+++ sankey qpm.R
@@ -1,7 +1,7 @@
 library(networkD3)
 
-#URL <- paste0("D:/Programming/Modelling/MPAFx/mpafx6.json")
-URL <- paste0("D:/Programming/Modelling/QPM_23062020/mpafx7.json")
+URL <- paste0("D:/Programming/Modelling/MPAFx/mpafx6.json")
+
 QPM <- jsonlite::fromJSON(URL)
 sankeyNetwork(Links = QPM$links, Nodes = QPM$nodes, Source = "source",
               Target = "target", Value = "value", NodeID = "name",
